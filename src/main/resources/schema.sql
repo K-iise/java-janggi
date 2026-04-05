f31709b442e0986ca3353c72f8225a9d4e146b96
K-iise <kimyungi1004@naver.com>
2026-04-05 20:21:59 +0900

    fix: 테이블 sql 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,9 +8,11 @@ CREATE TABLE IF NOT EXISTS game
 CREATE TABLE IF NOT EXISTS board_piece
 (
     id         int auto_increment primary key,
-    game_id    int references game (id),
-    type       varchar(10) not null,
-    team       varchar(5)  not null,
+    game_id    int         not null,
+    type       varchar(20) not null,
+    team       varchar(10) not null,
     x_position int         not null,
-    y_position int         not null
+    y_position int         not null,
+
+    CONSTRAINT fk_game_id FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE
 );
